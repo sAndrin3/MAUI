4bd37460ee14bc70f53806e18a53748171272599
sAndrin3 <sandrinemalanga2001@gmail.com>
2025-09-13 23:27:32 +0300

    Search bar, reusable card.

---
+++ src/components/common/BasicCard/styles.js
@@ -0,0 +1,8 @@
+export const cardStyles = {
+    width: '1050px',
+    margin: '50px auto',
+    borderRadius: '8px',
+    position: 'absolute',
+    left: '50%',
+    transform: 'translateX(50%)',
+};
